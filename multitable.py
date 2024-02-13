532e6bee35af0b26af285ff946eaaf10b4296f8d
kmxrj <159855066+kmxrj@users.noreply.github.com>
2024-02-12 18:47:36 -0800

    Update multitable.py

--- multitable.py
+++ multitable.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 from tabulate import tabulate
 
-#creating a list of list containing my multiplication table
+#creating a list of lists
 multi_table = []
 y = 1
 #set size of table
@@ -18,8 +18,8 @@ for j in range(size) :
     multi_table.append(table)
     y = y + 1
 
-#converts the raw dictionaries into a more usable pandas
+#converts the list of lists into a pandas data frame
 df = pd.DataFrame(multi_table)
 
-#prints out table
+#prints out the table
 print(tabulate(df, tablefmt = 'simple_grid', showindex = False)) 
